c6b150b3d1b47bda946ca55a306d07db8c735f32
Dragos Cirtorosan <56576401+qqrtos@users.noreply.github.com>
2021-12-13 18:35:40 +0200

    added vue router

--- photuris-frontend/src/main.js
+++ photuris-frontend/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import Test from './pages/Test.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Test },
+  // { path: '/login', component: Login },
+];
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
 
 new Vue({
+  router,
+  components: {
+    App
+  },
   render: h => h(App),
 }).$mount('#app')
