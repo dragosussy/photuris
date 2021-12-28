611aff48f8af92033f99ca854c108eff4f017ba9
Dragos Cirtorosan <56576401+qqrtos@users.noreply.github.com>
2021-12-28 18:35:35 +0200

    added base image architecture

---
+++ photuris-backend/photuris-backend/DbContext/Entities/Picture.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace photuris_backend.DbContext.Entities
+{
+    public class Picture
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Blob BinaryImageData { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public ulong SizeInBytes { get; set; }
+    }
+}
